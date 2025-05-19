3008df6331cf9691513d8182f2627223e8d86809
NarayanaGolla <narayana.golla.aws@gmail.com>
2025-05-19 10:05:13 +0530

    initial print statement

---
+++ basic/printsample/printSatementExamples.py
@@ -0,0 +1,21 @@
+# Example-1
+print("tharun")
+
+#Formatting with placeholders
+print("I'm going to inject %s here." %'tharun')
+print("I'm going to inject %s text here, and %s text here.%s" %('some','more','tharu'))
+
+x, y , z= 'some', 'more' , 'tharun'
+print("I'm going to inject %s text here, and %s text here. %s"%(x,y , z))
+
+
+#Format conversion methods.
+print('He said his name was %s.' %'Fred')
+print('He said his name was %r.' %'<Fred')
+
+
+print('I once caught a fish %s.' %'this \tbig')
+print('I once caught a fish %r.' %'this \tbig')
+
+print('I wrote %s programs today.' %3.75)
+print('I wrote %d programs today.' %3.75)
